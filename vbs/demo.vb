457123ee09d3acb32118c2f6f265d143172a7d8f
haoqis <haoqis@gmail.com>
2012-06-04 01:15:45 +0800

    .

--- vbs/demo.vb
+++ vbs/demo.vb
@@ -1,5 +1,6 @@
 Module demo
     Sub main()
+        'http://msdn.microsoft.com/en-us/library/system.environment.is64bitprocess
         Console.WriteLine(Environment.Is64BitOperatingSystem)
     End Sub
 End Module
