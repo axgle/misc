d8739fa5438c33b71a5a625daca3298fd9de4c7a
haoqis <haoqis@gmail.com>
2012-06-04 16:45:59 +0800

    .

--- vbs/file.vb
+++ vbs/file.vb
@@ -1,12 +1,24 @@
 'vbc file.vb && file.exe
 Module file
     Sub main()
-        FileOpen(1, "file.vb", OpenMode.Input)
+        Dim filename = "file.vb"
+        console.writeline(readALL(filename))
+
+        FileOpen(1, filename, OpenMode.Input)
         Dim cnt = 0
         Do While Not eof(1)
             cnt += 1
             console.writeline(cnt & ":" & LineInput(1))
         Loop
         FileClose(1)
+
     End Sub
+    Function readAll(ByVal filename)
+        FileOpen(1, filename, OpenMode.Input)
+
+        Dim d = inputString(1, LOF(1))
+
+        FileClose(1)
+        readAll = d
+    End Function
 End Module
