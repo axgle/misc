c3a042eb805dbf7b6634e1eae18dc2f3745359ee
haoqis <haoqis@gmail.com>
2012-06-05 02:02:37 +0800

    .

---
+++ vbs/md5.vb
@@ -0,0 +1,21 @@
+Imports System.Security.Cryptography
+Imports System.Text
+
+Module demo
+    Sub main()
+        echo(getMd5("1"))
+    End Sub
+    Sub echo(ByVal s)
+        console.WriteLine(s)
+    End Sub
+    'http://www.lob.cn/sl/sxzf/2062.shtml
+    Private Function getMd5(ByVal InPutString As String) As String
+        Dim Key As MD5 = MD5.Create
+        Dim Bytes() As Byte = Key.ComputeHash(Encoding.Default.GetBytes(InPutString))
+        Dim Sbuilter As New StringBuilder
+        For i As Integer = 0 To Bytes.Length - 1
+            Sbuilter.Append(Bytes(i).ToString("x2"))
+        Next
+        Return Sbuilter.ToString
+    End Function
+End Module
