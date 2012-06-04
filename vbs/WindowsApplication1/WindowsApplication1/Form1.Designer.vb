32cd56f9b343be192ec85350b628f3a1b14eb5eb
haoqis <haoqis@gmail.com>
2012-06-04 15:46:04 +0800

    top5

--- vbs/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ vbs/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -30,10 +30,18 @@ Partial Class Form1
         Me.TestTableAdapter = New WindowsApplication1.msDataSetTableAdapters.testTableAdapter()
         Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.MsDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TestBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.IdDataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.MsDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TestBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MsDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TestBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'BindingSource1
@@ -52,10 +60,10 @@ Partial Class Form1
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn})
         Me.DataGridView1.DataSource = Me.TestBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(99, 39)
+        Me.DataGridView1.Location = New System.Drawing.Point(1, 1)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(340, 150)
+        Me.DataGridView1.Size = New System.Drawing.Size(244, 247)
         Me.DataGridView1.TabIndex = 0
         '
         'TestBindingSource
@@ -79,11 +87,46 @@ Partial Class Form1
         Me.NameDataGridViewTextBoxColumn.HeaderText = "name"
         Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
         '
+        'DataGridView2
+        '
+        Me.DataGridView2.AutoGenerateColumns = False
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn1, Me.NameDataGridViewTextBoxColumn1})
+        Me.DataGridView2.DataSource = Me.TestBindingSource1
+        Me.DataGridView2.Location = New System.Drawing.Point(281, 1)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowTemplate.Height = 24
+        Me.DataGridView2.Size = New System.Drawing.Size(240, 247)
+        Me.DataGridView2.TabIndex = 1
+        '
+        'MsDataSetBindingSource
+        '
+        Me.MsDataSetBindingSource.DataSource = Me.MsDataSet
+        Me.MsDataSetBindingSource.Position = 0
+        '
+        'TestBindingSource1
+        '
+        Me.TestBindingSource1.DataMember = "test"
+        Me.TestBindingSource1.DataSource = Me.MsDataSetBindingSource
+        '
+        'IdDataGridViewTextBoxColumn1
+        '
+        Me.IdDataGridViewTextBoxColumn1.DataPropertyName = "id"
+        Me.IdDataGridViewTextBoxColumn1.HeaderText = "id"
+        Me.IdDataGridViewTextBoxColumn1.Name = "IdDataGridViewTextBoxColumn1"
+        '
+        'NameDataGridViewTextBoxColumn1
+        '
+        Me.NameDataGridViewTextBoxColumn1.DataPropertyName = "name"
+        Me.NameDataGridViewTextBoxColumn1.HeaderText = "name"
+        Me.NameDataGridViewTextBoxColumn1.Name = "NameDataGridViewTextBoxColumn1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(568, 260)
+        Me.Controls.Add(Me.DataGridView2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -91,6 +134,9 @@ Partial Class Form1
         CType(Me.MsDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TestBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MsDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TestBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -101,5 +147,10 @@ Partial Class Form1
     Friend WithEvents TestTableAdapter As WindowsApplication1.msDataSetTableAdapters.testTableAdapter
     Friend WithEvents IdDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents IdDataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TestBindingSource1 As System.Windows.Forms.BindingSource
+    Friend WithEvents MsDataSetBindingSource As System.Windows.Forms.BindingSource
 
 End Class
--- vbs/WindowsApplication1/WindowsApplication1/Form1.vb
+++ vbs/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -6,7 +6,15 @@
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: 这行代码将数据加载到表“MsDataSet.test”中。您可以根据需要移动或删除它。
+        ' Dim ds = Me.MsDataSet.Tables("test").DefaultView
+        'ds.Sort = "id desc"
+        'MsDataSet.testDataTable.
         Me.TestTableAdapter.Fill(Me.MsDataSet.test)
 
     End Sub
+
+    Private Sub TestBindingSource_CurrentChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TestBindingSource.CurrentChanged
+
+    End Sub
+ 
 End Class
